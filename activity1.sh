01dc82124aa87c0c6cb2ac3e8c7cc5150c7e0e61
Ruselle Daanoy <133234541+RuselleDaanoy@users.noreply.github.com>
2025-02-05 01:41:01 +0000

    activity1

---
+++ activity1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Declare a variable
+USER_NAME = "Name"
+USER_AGE = "Age"
+USER_PROGRAM = "Program"
+
+echo "Enter your name:"
+read USER_NAME
+echo "Enter your age:"
+read USER_AGE
+echo "Enter your program:"
+read USER_PROGRAM
+
+echo "Hello $USER_NAME, your age is $USER_AGE & your program is $USER_PROGRAM."
+
---
+++ sample.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Declare a variable
+USER_NAME = 'Student'
+USER_AGE = 'Age'
+USER_PROGRAM = 'Prog'
+
+echo "Enter your name:"
+read USER_NAME
+echo "Enter your age:"
+read USER_AGE
+echo "Enter your program:"
+read USER_PROGRAM
+
+echo "Hello $USER_NAME, this is your $USER_AGE & your program is $USER_PROGRAM."
+
